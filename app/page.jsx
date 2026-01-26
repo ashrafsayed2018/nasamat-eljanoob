ac908d2008f8e417999a361f4d729ee943ee9055
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2026-01-26 23:54:13 +0300

    first commit 3

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,25 @@
-import Hero from './components/home/Hero';
-import Commitment from './components/home/Commitment';
-import Solutions from './components/home/Solutions';
-import WhyUs from './components/home/WhyUs';
-import ValueAdded from './components/home/ValueAdded';
-import Stats from './components/home/Stats';
-import Testimonials from './components/home/Testimonials';
-import ContactCTA from './components/home/ContactCTA';
+import Commitment from './components/home/Commitment'
+import ContactCTA from './components/home/ContactCTA'
+import Hero from './components/home/Hero'
+import Solutions from './components/home/Solutions'
+import Stats from './components/home/Stats'
+import Testimonials from './components/home/Testimonials'
+import ValueAdded from './components/home/ValueAdded'
+import WhyUs from './components/home/WhyUs'
+import Portfolio from './portfolio/page.js'
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen">
+    <main className='flex flex-col min-h-screen'>
       <Hero />
       <Commitment />
       <Solutions />
       <WhyUs />
       <ValueAdded />
+      <Portfolio />
       <Stats />
       <Testimonials />
       <ContactCTA />
     </main>
-  );
+  )
 }
